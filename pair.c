372e0f4a2f7c0299995a1b627319b887bc0f19e2
AnnaNCS <anna.nefedenkova@gmail.com>
2020-09-24 16:25:19 -0700

    pair.c added

---
+++ pair.c
@@ -0,0 +1,12 @@
+#include "graph_node.h"
+#include "pair.h"
+#include "vector.h"
+#include <string.h>
+#include <stdlib.h>
+
+void pair_inti(char* name){
+
+    int len = strlen(name);
+    char* name_init = (char*) malloc (sizeof(char) * len);
+
+}
---
+++ pair.h
@@ -0,0 +1,13 @@
+#ifndef __PAIR__H__
+#define __PAIR__H__
+#include "vector.h"
+
+
+struct pair {
+    char* name_1;
+    char* name_2;
+};
+
+void name_init(char* name);
+
+#endif
